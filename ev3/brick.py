e3031a021ce3ca3d62169e91ea0bfa9936fe175e
asv009@post.uit.no <asv009@post.uit.no>
2014-03-18 19:22:30 +0100

    Added battery

--- ev3/brick.py
+++ ev3/brick.py
@@ -3,15 +3,51 @@ import error
 import asynchronous
 
 
+class Battery(object):  # if rechargable battery used
+    def __init__(self):
+        self.level_min = 7100
+        self.level_max = 8200
+        self.milli_voltage = 0
+
+        self.messages = {
+            0: "critical",
+            1: "low",
+            2: "ok",
+            3: "full"
+        }
+
+    def get_level(self):
+        if self.milli_voltage > self.level_max:
+            return 3
+        elif self.milli_voltage > (self.level_min - 100):
+            return 2
+        elif self.milli_voltage > self.level_min:
+            return 1
+        else:
+            return 0
+
+    def get_message(self, level=None):
+        if level is None:
+            level = self.get_level()
+        return self.messages[level]
+
+
 class Brick(object):
     def __init__(self, communication_object):
         """
         @param communication_object: Socket used for communicating with the brick
         @type communication_object: communication.Communication
         """
-        self._communication = communication_object
         self._message_handler = asynchronous.MessageHandler(communication_object)
         self._opened_ports = {}
+        self.battery = Battery()
+        self.refresh_battery()
+
+    def get_battery(self):
+        return self.battery
+
+    def refresh_battery(self):
+        self.battery.milli_voltage = self.send_command({"cla": "status", "cmd": "battery"})["data"]
 
     @property
     def get_opened_ports(self):
--- examples/test.py
+++ examples/test.py
@@ -2,16 +2,22 @@
 import ev3, time
 brick = ev3.connect_to_brick(address='10.0.1.1', port=9200)
 
+#
+# ev3_touch = ev3.EV3TouchSensor(brick, 1)
+# nxt_touch = ev3.NXTTouchSensor(brick, 4)
 
-ev3_touch = ev3.EV3TouchSensor(brick, 1)
-nxt_touch = ev3.NXTTouchSensor(brick, 4)
+# while True:
+#     start = time.time()
+#     ev3_touch.get_raw_data()
+#     end = time.time() - start
+#     print "took: ", end
+# brick.close()
 
-while True:
-    start = time.time()
-    ev3_touch.get_raw_data()
-    end = time.time() - start
-    print "took: ", end
-brick.close()
+battery = brick.get_battery()
+print battery.get_level()
+print battery.get_message()
+print battery.milli_voltage
+time.sleep(20)
 
-print ev3_touch
-print ev3_touch.get_name()
+# print ev3_touch
+# print ev3_touch.get_name()
