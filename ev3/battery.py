c08987b3a6c6022401d69d40ce1abf96629581ca
asv009@post.uit.no <asv009@post.uit.no>
2014-04-09 16:38:38 +0200

    Extracted the battery code into its own module

---
+++ ev3/battery.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+class Battery(object):
+    def __init__(self):
+        # if rechargeable battery used, different values if otherwise
+        self.level_min = 7100
+        self.level_max = 8200
+        self.milli_voltage = 0
+
+        self.messages = {
+            0: "critical",
+            1: "low",
+            2: "ok",
+            3: "full"
+        }
+
+    def get_level(self):
+        if self.milli_voltage > self.level_max:
+            return 3
+        elif self.milli_voltage > (self.level_min - 100):
+            return 2
+        elif self.milli_voltage > self.level_min:
+            return 1
+        else:
+            return 0
+
+    def get_message(self, level=None):
+        if level is None:
+            level = self.get_level()
+        return self.messages[level]
--- ev3/brick.py
+++ ev3/brick.py
@@ -1,39 +1,12 @@
 # -*- coding: utf-8 -*-
-import error
-import asynchronous
 import logging
 
+import error
+import asynchronous
+import battery
+
+
 _MODULE_LOGGER = logging.getLogger('ev3.brick')
-
-
-class Battery(object):
-    def __init__(self):
-        # if rechargeable battery used, different values if otherwise
-        self.level_min = 7100
-        self.level_max = 8200
-        self.milli_voltage = 0
-
-        self.messages = {
-            0: "critical",
-            1: "low",
-            2: "ok",
-            3: "full"
-        }
-
-    def get_level(self):
-        if self.milli_voltage > self.level_max:
-            return 3
-        elif self.milli_voltage > (self.level_min - 100):
-            return 2
-        elif self.milli_voltage > self.level_min:
-            return 1
-        else:
-            return 0
-
-    def get_message(self, level=None):
-        if level is None:
-            level = self.get_level()
-        return self.messages[level]
 
 
 class Brick(object):
@@ -44,7 +17,7 @@ class Brick(object):
         """
         self._message_handler = asynchronous.MessageHandler(communication_object)
         self._opened_ports = {}
-        self.battery = Battery()
+        self.battery = battery.Battery()
         self.hostname = ""
         self.refresh_battery()
         self.subscription = None
